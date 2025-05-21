53361de1ba63da2a9e987e2e144679df67081479
givenmphiwe <giggslive52@gmail.com>
2025-05-21 14:14:26 +0200

    The category screen for vistors

---
+++ src/screens/home/CategoryScreen.tsx
@@ -0,0 +1,227 @@
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import Entypo from "@expo/vector-icons/Entypo";
+import { useTheme } from "../../context/ThemeContext";
+import { fetchNews } from "../../api/NewsApi";
+import rootStore from "../../state/RootStore";
+import { useNavigation } from "@react-navigation/native";
+import { formatDate } from "../../utils/formatDate";
+
+const CategoryScreen = () => {
+  const { theme } = useTheme();
+  const { uiStore } = rootStore;
+  const navigation = useNavigation();
+  const [news, setNews] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [selectedTag, setSelectedTag] = useState<string | null>(null);
+  const [searchQuery, setSearchQuery] = useState("all");
+
+  const loadNews = async (query = searchQuery) => {
+    try {
+      setLoading(true);
+      const articles = await fetchNews({ query });
+      setNews(articles);
+      setError(null);
+    } catch (err: any) {
+      setError(err.message || "Something went wrong");
+    } finally {
+      setLoading(false);
+      setRefreshing(false);
+    }
+  };
+
+  useEffect(() => {
+    loadNews();
+  }, []);
+
+  const tags = [
+    "FridayMorning",
+    "CollegeColorsDay",
+    "Bitcoin",
+    "instagramDown",
+    "FridayFeeling",
+    "knifeTalk",
+    "ThursdayVibes",
+    "DigitalCurrency",
+    "Cryptocurrency",
+  ];
+
+  const handleArticlePress = (article: any) => {
+    uiStore.setSelectedArticle(article);
+    navigation.navigate("Article");
+  };
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await loadNews();
+  };
+
+  return (
+    <View className="flex-1 bg-white px-4 py-3">
+      {/* Search - fixed header */}
+      <View className="mb-4 mt-5 flex-row items-center bg-gray-100 rounded-xl px-3">
+        <TextInput
+          placeholder="Search"
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+          className="flex-1 py-3"
+        />
+        <TouchableOpacity onPress={() => loadNews(searchQuery)}>
+          <Entypo name="magnifying-glass" size={20} color="gray" />
+        </TouchableOpacity>
+      </View>
+
+      {/* Scrollable content */}
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        keyboardShouldPersistTaps="handled"
+        refreshControl={
+                  <RefreshControl
+                    refreshing={refreshing}
+                    onRefresh={onRefresh}
+                    tintColor="#007AFF"
+                    colors={["#007AFF"]}
+                  />
+                }
+      >
+        {/* Popular Tags */}
+        <View className="mb-6">
+          <View className="flex-row justify-between items-center mb-2">
+            <Text className="text-lg font-semibold">Popular Tags</Text>
+            <Text className="text-blue-500 font-bold">View All</Text>
+          </View>
+
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <View className="flex-row flex-wrap">
+              {tags.map((tag, index) => (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => {
+                    setSelectedTag(tag);
+                    setSearchQuery(tag);
+                    loadNews(tag);
+                  }}
+                  className={`px-2 py-1 mr-2 mb-2 rounded ${
+                    selectedTag === tag ? "bg-blue-500" : "bg-gray-200"
+                  }`}
+                >
+                  <Text
+                    className={`text-xs ${
+                      selectedTag === tag ? "text-white" : "text-gray-700"
+                    }`}
+                  >
+                    #{tag}
+                  </Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </ScrollView>
+        </View>
+
+        {/* Latest News */}
+        <View className="h-px bg-gray-300 mb-1" />
+        <View className="mb-6">
+          <View className="flex-row justify-between items-center mb-2">
+            <Text className="text-lg font-semibold">Latest News</Text>
+            <Text className="text-blue-500 font-bold">View All</Text>
+          </View>
+          {loading ? (
+            <ActivityIndicator />
+          ) : (
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              {news.slice(0, 5).map((item, index) => (
+                <TouchableOpacity
+                  key={index}
+                  className="mr-4 w-64"
+                  onPress={() => handleArticlePress(item)}
+                >
+                  <Image
+                    source={{
+                      uri: item.urlToImage || "https://via.placeholder.com/150",
+                    }}
+                    className="h-36 rounded-xl mb-2"
+                  />
+                  <Text
+                    className="font-medium text-base"
+                    style={{ color: theme.text, lineHeight: 15 }}
+                    numberOfLines={2}
+                  >
+                    {item.title}{" "}
+                  </Text>
+                  <View className="flex-row items-center space-x-1">
+                    <Text
+                      className="text-blue-500 text-xs font-light"
+                      numberOfLines={1}
+                      ellipsizeMode="tail"
+                      style={{ maxWidth: "50%" }}
+                    >
+                      {item.source?.name || "Source"}
+                    </Text>
+                    <Text className="text-gray-400 text-xs ">
+                      {formatDate(item.publishedAt)}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+          )}
+        </View>
+
+        {/* Recommendation Topics */}
+        <View className="h-px bg-gray-300 mb-4" />
+        <View className="mb-6">
+          <View className="flex-row justify-between items-center mb-2">
+            <Text className="text-lg font-semibold">Recomendation Topic</Text>
+            <Text className="text-blue-500 font-bold">View All</Text>
+          </View>
+          {news.slice(5, 10).map((item, index) => (
+            <TouchableOpacity
+              onPress={() => handleArticlePress(item)}
+              key={index}
+              className="flex-row mb-4 items-center"
+            >
+              <View className="flex-1">
+                <Text
+                  className="font-medium"
+                  style={{ color: theme.text, lineHeight: 15 }}
+                  numberOfLines={2}
+                >
+                  {item.title}
+                </Text>
+
+                <View className="flex-row items-center justify-between">
+                  <Text className="text-blue-500 text-xs font-light">
+                    {item.source?.name || "Source"}
+                  </Text>
+
+                  <Text className="text-gray-400 text-xs font-light mr-6">
+                    {formatDate(item.publishedAt)}
+                  </Text>
+                </View>
+              </View>
+              <Image
+                source={{
+                  uri: item.urlToImage || "https://via.placeholder.com/100",
+                }}
+                className="w-20 h-20 rounded-lg mr-3"
+              />
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default CategoryScreen;
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -13,6 +13,10 @@ import { useTheme } from "../../context/ThemeContext";
 import { fetchNews } from "../../api/NewsApi";
 import rootStore from "../../state/RootStore";
 import { useNavigation } from "@react-navigation/native";
+import { auth } from "../../../firebaseConfig";
+import { getDatabase, ref, onValue, update } from "firebase/database";
+import { onAuthStateChanged } from "firebase/auth";
+import CategoryScreen from "./CategoryScreen";
 
 const HomeScreen = () => {
   const { theme } = useTheme();
@@ -21,6 +25,7 @@ const HomeScreen = () => {
   const [news, setNews] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
+  const [role, setRole] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
 
   const loadNews = async () => {
@@ -45,6 +50,23 @@ const HomeScreen = () => {
     loadNews();
   }, [uiStore.categories]);
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        const db = getDatabase();
+        const userRef = ref(db, `users/${uid}`);
+        onValue(userRef, (snapshot) => {
+          const data = snapshot.val();
+          if (data) {
+            setRole(data.role || "media");
+            // other setUserData, etc.
+          }
+        });
+      }
+    });
+  }, [uiStore.categories]);
+
   const onRefresh = async () => {
     setRefreshing(true);
     await loadNews();
@@ -55,72 +77,75 @@ const HomeScreen = () => {
     navigation.navigate("Article");
   };
 
-  return (
-    <View className="flex-1" style={{ backgroundColor: theme.background }}>
-      <Header onTabSelect={handleTabSelect} />
-      {loading && !refreshing ? (
-        <ActivityIndicator size="large" color="#007AFF" className="mt-6" />
-      ) : error ? (
-        <Text className="text-red-600 p-4">{error}</Text>
-      ) : (
-        <ScrollView
-          contentContainerStyle={{ padding: 16 }}
-          refreshControl={
-            <RefreshControl
-              refreshing={refreshing}
-              onRefresh={onRefresh}
-              tintColor="#007AFF"
-              colors={["#007AFF"]}
-            />
-          }
-        >
-          {news.map((article, index) => (
-            <TouchableOpacity
-              onPress={() => handleArticlePress(article)}
-              key={index}
-              className="rounded-xl overflow-hidden mb-4 shadow-xl"
-              style={{
-                backgroundColor: theme.background,
-                borderRadius: 12,
-                marginBottom: 16,
-                overflow: "hidden",
-                shadowColor: "#000",
-                shadowOffset: { width: 0, height: 2 },
-                shadowOpacity: 0.2,
-                shadowRadius: 4,
-                elevation: 4,
-              }}
-            >
-              {article.urlToImage ? (
-                <Image
-                  source={{ uri: article.urlToImage }}
-                  className="w-full h-52"
-                  resizeMode="cover"
-                />
-              ) : null}
-              <View className="p-4">
-                <Text
-                  className="text-lg font-bold mb-1"
-                  style={{ color: theme.text, lineHeight: 23 }}
-                >
-                  {article.title}
-                </Text>
-                <View className="flex-row font-bold justify-between items-center mb-1">
-                  <Text className="text-xs" style={{ color: theme.text }}>
-                    Published:{" "}
-                    {new Date(article.publishedAt).toLocaleDateString()}
-                  </Text>
-                </View>
-                <Text className=" mb-4" style={{ color: theme.secondary }}>
-                  {article.description}
+ return (
+  <View className="flex-1" style={{ backgroundColor: theme.background }}>
+   {role === "media" && <Header onTabSelect={handleTabSelect} />}
+    {loading && !refreshing ? (
+      <ActivityIndicator size="large" color="#007AFF" className="mt-6" />
+    ) : error ? (
+      <Text className="text-red-600 p-4">{error}</Text>
+    ) : role === "media" ? (
+      <ScrollView
+        contentContainerStyle={{ padding: 16 }}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            tintColor="#007AFF"
+            colors={["#007AFF"]}
+          />
+        }
+      >
+        {news.map((article, index) => (
+          <TouchableOpacity
+            onPress={() => handleArticlePress(article)}
+            key={index}
+            className="rounded-xl overflow-hidden mb-4 shadow-xl"
+            style={{
+              backgroundColor: theme.background,
+              borderRadius: 12,
+              marginBottom: 16,
+              overflow: "hidden",
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 0.2,
+              shadowRadius: 4,
+              elevation: 4,
+            }}
+          >
+            {article.urlToImage && (
+              <Image
+                source={{ uri: article.urlToImage }}
+                className="w-full h-52"
+                resizeMode="cover"
+              />
+            )}
+            <View className="p-4">
+              <Text
+                className="text-lg font-bold mb-1"
+                style={{ color: theme.text, lineHeight: 23 }}
+              >
+                {article.title}
+              </Text>
+              <View className="flex-row justify-between items-center mb-1">
+                <Text className="text-xs" style={{ color: theme.text }}>
+                  Published:{" "}
+                  {new Date(article.publishedAt).toLocaleDateString()}
                 </Text>
               </View>
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
-      )}
-    </View>
-  );
-};
+              <Text className="mb-4" style={{ color: theme.secondary }}>
+                {article.description}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    ) : (
+     <CategoryScreen/>
+    )}
+  </View>
+);
+}
+
 
 export default HomeScreen;
